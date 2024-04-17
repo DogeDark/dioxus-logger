d924e8110acebf99e22a942a9661a5b81bf10193
Miles Murgaw <milesmurgaw@gmail.com>
2024-04-16 21:17:20 -0400

    feat: 0.5 tracing

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <div align="center">
   <h1>📡 Dioxus Logger 🛰️</h1>
-  <p><strong>A logging utility to provide a standard interface whether you're targetting web, desktop, or mobile with Dioxus.</strong></p>
+  <p><strong>A logging utility to provide a standard interface whether you're targetting web desktop, fullstack, and more.</strong></p>
 </div>
 
 <div align="center">
@@ -23,68 +23,41 @@
 
 -----
 
-`dioxus-logger` is a (planned-to-be) feature-rich logger for [Dioxus](https://dioxuslabs.com/) that uses the [log](https://crates.io/crates/log) crate to provide a standard interface for all your logging needs.
+`dioxus-logger` is a basic cross-platform facade for logging in [Dioxus](https://dioxuslabs.com/) that uses the [tracing](https://crates.io/crates/tracing) crate.
 
-**Current & Planned Features**
-- [x] stdio logging (Web, Desktop) - Mobile to come
-- [x] Custom log format - Basic Implementation Finished
-- [x] Timestamps 
-- [ ] Sending logs over HTTP to an API
-- [ ] Logging to a file
-- [ ] Logging to [Sentry](https://sentry.io/)?
 
-**This library is not finished. Breaking changes may occur.**
 ```rust
 use dioxus::prelude::*;
-use log::{info, LevelFilter};
-
+use tracing::{Level, info};
+ 
 fn main() {
-    dioxus_logger::init(LevelFilter::Info).expect("failed to init logger");
-    dioxus_web::launch(app);
+  dioxus_logger::init(Level::INFO).expect("logger failed to init");
+  launch(App);
 }
 
-fn app(cx: Scope) -> Element {
-    info!("my app component just rendered!");
-    cx.render(rsx!(p { "Hello, Dioxus!" }))
+#[component]
+fn App() -> Element {
+  info!("App rendered");
+  rsx! {
+      "hi"
+  }
 }
 ```
 
 ## Platform Support
-Dioxus logger will eventually support every target that Dioxus does. Currently only web and desktop platforms are supported.
+Dioxus logger will eventually support every target that Dioxus does. Currently mobile is not supported.
 
 ## Installation
 You can add `dioxus-logger` to your application by adding it to your dependencies.
 ```toml
 [dependencies]
-dioxus-logger = "x.x.x"
+dioxus-logger = "0.5"
 ```
 
-To use dioxus-logger as is, add this line before launching Dioxus, replacing ``LevelFilter::Info`` with your preferred log level.
-```rs
-dioxus_logger::init(LevelFilter::Info).expect("failed to init logger");
-```
-
-## Custom Format
-Initialize dioxus-logger using the builder:
-```rs
-dioxus_logger::DioxusLogger::new(LevelFilter::Info)
-        .use_format("[{LEVEL}] {PATH} - {ARGS}")
-        .build()
-        .expect("failed to init logger");
-```
-The available options for ``use_format`` are:
-- LEVEL - The log severity.
-- PATH - Which crate emitted the log.
-- ARGS - The text that was logged.
-- TIMESTAMP - A timestamp of when the log was emitted. Requires the ``timestamps`` feature.
-
-Surround the option with ``{}`` in the ``use_format`` method.
 
 ## License
 This project is licensed under the [MIT license].
 
 [mit license]: ./LICENSE
 
-Every contribution intentionally submitted
-for inclusion in `dioxus-logger` by you, shall be licensed as MIT, without any additional
-terms or conditions.
+Every contribution intentionally submitted for inclusion in `dioxus-logger` by you, shall be licensed as MIT, without any additional terms or conditions.
--- src/lib.rs
+++ src/lib.rs
@@ -1,103 +1,50 @@
-use log::{LevelFilter, SetLoggerError};
+use tracing::{
+    subscriber::{set_global_default, SetGlobalDefaultError}, Level,
+};
 
-/// The log level. E.g. ``INFO``
-const LEVEL: &str = "{LEVEL}";
-/// The file's path or crate name. E.g. ``dioxus-testing``
-const PATH: &str = "{PATH}";
-/// The arguments passed through the log macro.
-/// E.g. the args from ``info!("hello")`` will be ``"hello"``
-const ARGS: &str = "{ARGS}";
-/// A timestamp of when the log was generated.
-const TIMESTAMP: &str = "{TIMESTAMP}";
+/// Initialize `dioxus-logger` with a specified max filter.
+/// Generally it is best to initialize the logger before launching your Dioxus app.
+/// Works on Web, Desktop, Fullstack, Liveview, and TUI.
+/// 
+/// # Example
+/// 
+/// ```rust,ignore
+/// use dioxus::prelude::*;
+/// use tracing::{Level, info};
+/// 
+/// fn main() {
+///     dioxus_logger::init(Level::INFO).expect("logger failed to init");
+///     launch(App);
+/// }
+/// 
+/// #[component]
+/// fn App() -> Element {
+///     info!("App rendered");
+///     rsx! {
+///         "hi"
+///     }
+/// }
+/// ```
+pub fn init(level: Level) -> Result<(), SetGlobalDefaultError> {
+    #[cfg(target_arch = "wasm32")]
+    {
+        use tracing_subscriber::layer::SubscriberExt;
+        use tracing_subscriber::Registry;
 
-/// The primary logging struct that contains all runtime configuration.
-pub struct DioxusLogger {
-    level_filter: LevelFilter,
-    format: &'static str,
-}
+        let layer_config = tracing_wasm::WASMLayerConfigBuilder::new().set_max_level(level).build();
+        let layer = tracing_wasm::WASMLayer::new(layer_config);
+        let reg = Registry::default().with(layer);
 
-impl DioxusLogger {
-    /// Create a new [`DioxusLogger`] struct to configure and build.
-    pub fn new(level_filter: LevelFilter) -> Self {
-        let format = "[{LEVEL}] {PATH} - {ARGS}";
-
-        #[cfg(feature = "timestamps")]
-        let format = "[{TIMESTAMP} | {LEVEL}] {PATH} - {ARGS}";
-
-        Self {
-            level_filter,
-            format,
-        }
+        console_error_panic_hook::set_once();
+        set_global_default(reg)
     }
 
-    /// Builds and initializes the logger with [`log`]
-    pub fn build(self) -> Result<(), SetLoggerError> {
-        let level = self.level_filter.clone();
-        log::set_boxed_logger(Box::new(self)).map(|()| log::set_max_level(level))
-    }
+    #[cfg(not(target_arch = "wasm32"))]
+    return {
+        let sub = tracing_subscriber::FmtSubscriber::builder()
+            .with_max_level(level)
+            .finish();
 
-    /// Allows you to define a custom format.
-    ///
-    /// The available options are `{LEVEL}`, `{PATH}`, `{ARGS}` and `{TIMESTAMP}`
-    ///
-    /// Providing the format of `[{LEVEL}] {PATH} - {ARGS}]` will return something like `[INFO] dioxus_testing - this is my log message`
-    pub fn use_format(self, format: &'static str) -> Self {
-        Self {
-            level_filter: self.level_filter,
-            format
-        }
-    }
-}
-
-impl log::Log for DioxusLogger {
-    fn enabled(&self, metadata: &log::Metadata) -> bool {
-        metadata.level() <= self.level_filter
-    }
-
-    fn log(&self, record: &log::Record) {
-        if !self.enabled(record.metadata()) {
-            return;
-        }
-
-        // This is cursed
-        let formatted = self
-            .format
-            // LEVEL
-            .replace(LEVEL, record.level().as_str())
-            .to_owned()
-            // PATH
-            .replace(PATH, record.module_path().unwrap_or(""))
-            .to_owned()
-            // ARGS
-            .replace(ARGS, record.args().to_string().as_str());
-
-        #[cfg(feature = "timestamps")]
-        let formatted = format_timestamp(formatted);
-
-        #[cfg(all(
-            not(target_family = "wasm"),
-            not(target_family = "android"),
-            not(target_family = "ios")
-        ))]
-        println!("{formatted}");
-
-        #[cfg(target_family = "wasm")]
-        web_sys::console::log_1(&formatted.into());
-    }
-
-    fn flush(&self) {}
-}
-
-#[cfg(feature = "timestamps")]
-fn format_timestamp(formatted: String) -> String {
-    let timestamp = time::OffsetDateTime::now_utc();
-    formatted
-        .to_owned()
-        .replace(TIMESTAMP, timestamp.to_string().as_str())
-}
-
-/// Initialize `log` and `dioxus-logger` with a specified filter.
-/// For more advanced configuration, build the logger through the [`DioxusLogger`] struct.
-pub fn init(level_filter: LevelFilter) -> Result<(), SetLoggerError> {
-    DioxusLogger::new(level_filter).build()
+        set_global_default(sub)
+    };
 }
